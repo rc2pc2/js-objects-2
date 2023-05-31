0732285b54983d92b374d815ad18bba80bef8a61
rc2pc2 <petriccarcc@gmail.com>
2023-05-31 13:08:27 +0200

    small exercise reversing case on words

--- js/myscript.js
+++ js/myscript.js
@@ -11,33 +11,56 @@
 // // § const hp = auto.hp;
 // const {brand, model, doors, hp} = auto;
 
-const obj = {
-   gino : 'ginetti',
-   gina : 'ginettini',
-   ginetto : 'giuni'
-};
+// const obj = {
+//    gino : 'ginetti',
+//    gina : 'ginettini',
+//    ginetto : 'giuni'
+// };
 
-const array = [1,2,3,4,5,6,8];
+// const array = [1,2,3,4,5,6,8];
 
-console.log(obj);
-const oggettoCopiato = {
-   ...obj,
-   copiato: true
-};
+// console.log(obj);
+// const oggettoCopiato = {
+//    ...obj,
+//    copiato: true
+// };
 
-console.log(oggettoCopiato);
+// console.log(oggettoCopiato);
 
 
-const oggettoCopiatoMale = {
-   obj : obj,
-   copiato: true
-};
+// const oggettoCopiatoMale = {
+//    obj : obj,
+//    copiato: true
+// };
 
-console.log(oggettoCopiatoMale);
+// console.log(oggettoCopiatoMale);
 
-console.log(array);
-const nuovoArraySbaglieto = [array];
-const nuovoArray = [...array];
+// console.log(array);
+// const nuovoArraySbaglieto = [array];
+// const nuovoArray = [...array];
 
-console.log(nuovoArray, nuovoArraySbaglieto);
+// console.log(nuovoArray, nuovoArraySbaglieto);
 
+
+
+
+/**
+ *
+ * /**
+ *
+ *
+ *
+A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l'iniziale maiuscola.
+
+Es: ['pippo', 'PLUTO', 'PapERino'] => ['Pippo', 'Pluto', 'Paperino']
+
+ */
+const strings = ['pippo', 'PLUTO', 'PlUtO', 'paPERINA', 'pluTINO','TOPOlino', 'topolinA'];
+const convertedStrings = [];
+
+strings.forEach((word) => {
+   const newString = word.charAt(0).toUpperCase() + word.substring(1).toLowerCase();
+   convertedStrings.push(newString);
+})
+
+console.log(convertedStrings);
